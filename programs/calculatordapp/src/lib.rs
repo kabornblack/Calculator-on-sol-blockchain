b6a5be5b9a54f1e093f9685760ed3360765617aa
kabornblack <shaibu.kabiru@email.com>
2025-07-19 07:59:40 +0000

    Added mathmathical functionalities

--- programs/calculatordapp/src/lib.rs
+++ programs/calculatordapp/src/lib.rs
@@ -6,18 +6,79 @@ declare_id!("HgqwqYTqTvDYbarZCBijebg5wCzqkjSYxT4T1vbH75he");
 pub mod calculatordapp {
     use super::*;
 
-    pub fn create(ctx: Context<Create>, init_message: String) -> {
-        let calculator = &mut ctx.account.calculator;
-        calculator.greeting = init_message;
+pub fn create(ctx: Context<Create>, init_message: String) -> Result<()> {
+    let calculator = &mut ctx.accounts.calculator;
+    calculator.greeting = init_message;
+    calculator.result = 0;
+    calculator.remainder = 0;
+    Ok(())
+    }
+
+    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 + num2;
         Ok(())
     }
+
+    pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 - num2;
+        Ok(())
+    }
+
+    pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 * num2;
+        Ok(())
+    }
+
+    pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 / num2;
+        calculator.remainder = num1 % num2;
+        Ok(())
+    }
+
 }
 
-#derive(Accounts)
-pub struct create<'info> {
-    #[account(init, payer=user, space=264)]
-    pub calculator: Account<'info, Calculator>
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 264)]
+    pub calculator: Account<'info, Calculator>,
     #[account(mut)]
     pub user: Signer<'info>,
-    pub system_program: Program<'info, System>
+    pub system_program: Program<'info, System>,
 }
+
+#[derive(Accounts)]
+pub struct Addition<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+}
+
+#[derive(Accounts)]
+pub struct Subtraction<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+}
+
+#[derive(Accounts)]
+pub struct Multiplication<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+}
+
+#[derive(Accounts)]
+pub struct Division<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+}
+
+#[account]
+pub struct Calculator {
+    pub greeting: String,
+    pub result: i64,
+    pub remainder: i64,
+}
+
--- tests/calculatordapp.ts
+++ tests/calculatordapp.ts
@@ -1,16 +1,63 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Calculatordapp } from "../target/types/calculatordapp";
+const anchor = require("@coral-xyz/anchor");
+const assert = require("assert");
+const { SystemProgram } = anchor.web3;
 
 describe("calculatordapp", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.local();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.calculatordapp as Program<Calculatordapp>;
+  const calculator = anchor.web3.Keypair.generate();
+  const program = anchor.workspace.Calculatordapp;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+  it("Create a calculator", async () => {
+    await program.methods.create("Welcome to Solana").accounts({
+      calculator: calculator.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    }).signers([calculator]).rpc();
+
+    const account = await program.account.calculator.fetch(calculator.publicKey);
+    assert.ok(account.greeting === "Welcome to Solana");
+  })
+
+  it("Adds two numbers", async() => {
+    await program.rpc.add(new anchor.BN(2), new anchor.BN(3), {
+      accounts: {
+        calculator: calculator.publicKey
+      }
+    })
+    const account = await program.account.calculator.fetch(calculator.publicKey);
+    assert.ok(account.result.eq(new anchor.BN(5)))
+  })
+
+  it("Subtracts two numbers", async() => {
+    await program.rpc.subtract(new anchor.BN(5), new anchor.BN(2), {
+      accounts: {
+        calculator: calculator.publicKey
+      }
+    })
+    const account = await program.account.calculator.fetch(calculator.publicKey);
+    assert.ok(account.result.eq(new anchor.BN(3)))
+  })
+
+  it("Multiplies two numbers", async() => {
+    await program.rpc.multiply(new anchor.BN(3), new anchor.BN(2), {
+      accounts: {
+        calculator: calculator.publicKey
+      }
+    })
+    const account = await program.account.calculator.fetch(calculator.publicKey);
+    assert.ok(account.result.eq(new anchor.BN(6)))
+  })
+
+    it("Divides two numbers", async() => {
+    await program.rpc.divide(new anchor.BN(10), new anchor.BN(3), {
+      accounts: {
+        calculator: calculator.publicKey
+      }
+    })
+    const account = await program.account.calculator.fetch(calculator.publicKey);
+    assert.ok(account.result.eq(new anchor.BN(3)));
+    assert.ok(account.remainder.eq(new anchor.BN(1)));
+  })
 });
